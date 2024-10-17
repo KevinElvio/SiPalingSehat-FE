d1118bd117b9130c91f2ef62b05ef374243c5386
haris ardian <harisfirman387@gmail.com>
2024-10-17 13:18:32 +0700

    add landingpage

---
+++ LandingPage.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stunting Prevention</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-gray-50">
+    <header class="bg-white relative">
+        <div class="container mx-auto px-4 xl:max-w-screen-xl flex flex-col lg:flex-row lg:justify-between lg:items-center py-8">
+            <div class="flex flex-col items-start">
+                <div class="mb-4">
+                    <img src="img/logo.png" alt="Left Logo" class="w-16 h-16">
+                </div>
+                <div class="text-left">
+                    <h1 class="text-3xl lg:text-5xl font-bold text-blue-500">CEGAH DAN KENALI GEJALA STUNTING</h1>
+                    <p class="mt-4 text-lg lg:text-xl text-gray-700">
+                        Panduan Lengkap untuk Mencegah <span class="text-blue-500">Stunting</span> dan Mengenali Gejala Dini
+                        <span class="text-blue-500">Gizi</span> Buruk pada <span class="text-blue-500">Pertumbuhan Anak</span>
+                    </p>
+                    <a href="#" class="mt-6 inline-block bg-blue-500 text-white px-6 py-3 rounded-full text-lg">MULAI</a>
+                </div>
+            </div>
+            <div class="mt-8 lg:mt-0 lg:ml-8 lg:flex-shrink-0 lg:block hidden">
+                <img src="img/logo-header.png" alt="Right Logo" class="     2xl:w-96  lg:h-auto">
+            </div>
+
+        </div>
+    </header>
+
+    <section class="py-16">
+        <div class="container mx-auto px-4 xl:max-w-screen-xl">
+            <h2 class="text-2xl lg:text-3xl font-bold text-center mb-8">Gejala Stunting</h2>
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 text-center">
+                <div class="bg-white shadow p-6 rounded-lg">
+                    <p class="text-lg font-semibold">Tumbuh kembang terhambat</p>
+                </div>
+                <div class="bg-white shadow p-6 rounded-lg">
+                    <p class="text-lg font-semibold">Wajah tampak lebih muda dari anak seusianya</p>
+                </div>
+                <div class="bg-white shadow p-6 rounded-lg">
+                    <p class="text-lg font-semibold">Berat badan tidak naik bahkan cenderung menurun</p>
+                </div>
+                <div class="bg-white shadow p-6 rounded-lg">
+                    <p class="text-lg font-semibold">Anak mudah terkena penyakit infeksi</p>
+                </div>
+                <div class="bg-white shadow p-6 rounded-lg">
+                    <p class="text-lg font-semibold">Kesemparan fisik dan motorik terlambat</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-blue-100 py-16">
+        <div class="container mx-auto px-4 xl:max-w-screen-xl">
+            <h2 class="text-2xl lg:text-3xl font-bold text-center mb-8">Pencegahan Stunting</h2>
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 text-center">
+                <div class="bg-white shadow p-6 rounded-lg">
+                    <p class="text-lg font-semibold">Berikan asi ekslusif hingga 6 bulan</p>
+                </div>
+                <div class="bg-white shadow p-6 rounded-lg">
+                    <p class="text-lg font-semibold">Pemberian MPASI sehat dan bergizi</p>
+                </div>
+                <div class="bg-white shadow p-6 rounded-lg">
+                    <p class="text-lg font-semibold">Rutin imunisasi dan berikan vitamin tambahan</p>
+                </div>
+                <div class="bg-white shadow p-6 rounded-lg">
+                    <p class="text-lg font-semibold">Ciptakan lingkungan bersih dan sehat</p>
+                </div>
+                <div class="bg-white shadow p-6 rounded-lg">
+                    <p class="text-lg font-semibold">Rutin pantau pertumbuhan anak</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="py-16">
+        <div class="container mx-auto px-4 xl:max-w-screen-xl">
+            <h2 class="text-2xl lg:text-3xl font-bold text-center mb-8">Artikel Pilihan</h2>
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+                <div class="bg-white shadow rounded-lg overflow-hidden">
+                    <img src="img/artikel-1.jpg" alt="Artikel 1" class="w-full h-48 object-cover">
+                    <div class="p-4">
+                        <h3 class="text-lg font-semibold mb-2">Skrining Hipotiroid Kongenital (SHK) untuk Bayi Sehat</h3>
+                        <p class="text-gray-600">Memiliki bayi yang lahir sehat merupakan suatu kebahagiaan yang tiada tara untuk ayah dan bunda, setelah itu pun sebagai...</p>
+                        <a href="#" class="block mt-4 text-blue-500">Baca Selengkapnya</a>
+                    </div>
+                </div>
+                <div class="bg-white shadow rounded-lg overflow-hidden">
+                    <img src="img/artikel-2.jpg" alt="Artikel 2" class="w-full h-48 object-cover">
+                    <div class="p-4">
+                        <h3 class="text-lg font-semibold mb-2">1000 HPK Kunci Cegah Stunting</h3>
+                        <p class="text-gray-600">Masalah stunting masih menjadi episode panjang masalah kesehatan balita di Indonesia. Stunting adalah kondisi gagal tumbuh...</p>
+                        <a href="#" class="block mt-4 text-blue-500">Baca Selengkapnya</a>
+                    </div>
+                </div>
+                <div class="bg-white shadow rounded-lg overflow-hidden">
+                    <img src="img/artikel-3.jpg" alt="Artikel 3" class="w-full h-48 object-cover">
+                    <div class="p-4">
+                        <h3 class="text-lg font-semibold mb-2">Cegah dengan pola asuh baik</h3>
+                        <p class="text-gray-600">Menurut Teori klasik H. L. Bloom (1974) gaya hidup, lingkungan (sosial, ekonomi, politik, budaya), pelayanan kesehatan, dan faktor...</p>
+                        <a href="#" class="block mt-4 text-blue-500">Baca Selengkapnya</a>
+                    </div>
+                </div>
+                <div class="bg-white shadow rounded-lg overflow-hidden">
+                    <img src="img/artikel-4.jpg" alt="Artikel 4" class="w-full h-48 object-cover">
+                    <div class="p-4">
+                        <h3 class="text-lg font-semibold mb-2">Pakar UNDIP Rekomendasikan Pencegahan Stunting</h3>
+                        <p class="text-gray-600">Dewan Profesor Senat Akademik Universitas Diponegoro (UNDIP) menyelenggarakan kegiatan Webinar “UNDIP’s...</p>
+                        <a href="#" class="block mt-4 text-blue-500">Baca Selengkapnya</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+     <section class="py-16">
+        <div class="container mx-auto px-4 xl:max-w-screen-xl">
+            <h2 class="text-2xl lg:text-3xl font-bold text-center mb-8">Berita Pilihan</h2>
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+                <div class="bg-white shadow rounded-lg overflow-hidden">
+                    <img src="img/berita-1.jpg" alt="Artikel 1" class="w-full h-48 object-cover">
+                    <div class="p-4">
+                        <h3 class="text-lg font-semibold mb-2">7 Ilmiah Stunting</h3>
+                        <p class="text-gray-600">Sebuah kajian mengungkapkan bahwa sanitasi yang layak dan akses terhadap air bersih menjadi faktor inti dalam pencegahan stunting pada anak-anak.</p>
+                        <a href="#" class="block mt-4 text-blue-500">Baca Selengkapnya</a>
+                    </div>
+                </div>
+                <div class="bg-white shadow rounded-lg overflow-hidden">
+                    <img src="img/berita-2.jpg" alt="Artikel 2" class="w-full h-48 object-cover">
+                    <div class="p-4">
+                        <h3 class="text-lg font-semibold mb-2">Perangi Stunting Papua-Aceh</h3>
+                        <p class="text-gray-600">Merespons hasil Survei Kesehatan Indonesia (SKI) 2023 yang menunjukkan bahwa stunting masih menjadi masalah...</p>
+                        <a href="#" class="block mt-4 text-blue-500">Baca Selengkapnya</a>
+                    </div>
+                </div>
+                <div class="bg-white shadow rounded-lg overflow-hidden">
+                    <img src="img/berita-3.jpg" alt="Artikel 3" class="w-full h-48 object-cover">
+                    <div class="p-4">
+                        <h3 class="text-lg font-semibold mb-2">FOI 1000 Pangan</h3>
+                        <p class="text-gray-600">Foodbank of Indonesia (FOI) berkomitmen membawa kemerdekaan yang sejati, yakni kemerdekaan dari kelaparan dan malnutrisi. Kongres yang... </p>
+                        <a href="#" class="block mt-4 text-blue-500">Baca Selengkapnya</a>
+                    </div>
+                </div>
+                <div class="bg-white shadow rounded-lg overflow-hidden">
+                    <img src="img/berita-4.jpg" alt="Artikel 4" class="w-full h-48 object-cover">
+                    <div class="p-4">
+                        <h3 class="text-lg font-semibold mb-2">Satu telur sehari</h3>
+                        <p class="text-gray-600">Alfamart berupaya untuk memberikan kontribusi dalam penurunan prevalensi angka stunting di Indonesia melalui program Satu Telur Sehari. Program berkelanjutan..</p>
+                        <a href="#" class="block mt-4 text-blue-500">Baca Selengkapnya</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer class="bg-blue-900 py-12 text-white">
+        <div class="container mx-auto px-4 xl:max-w-screen-xl flex justify-between">
+            <div>
+                <h2 class="text-2xl font-semibold">SiPalingSehat</h2>
+                <p class="mt-4 text-sm max-w-xs">
+                    Pantauan gejala stunting pada anak usia dini dan perawatan stunting secara bertahap melalui SiPalingSehat.
+                </p>
+            </div>
+
+            <div class="text-right">
+                <h2 class="text-xl font-semibold mb-4">Ikuti Kami</h2>
+                <ul class="space-y-2">
+                    <li><a href="#" class="hover:underline">About Us</a></li>
+                    <li><a href="mailto:someone@example.com" class="hover:underline">Email</a></li>
+                    <li><a href="tel:+123456789" class="hover:underline">Phone</a></li>
+                    <li><a href="#" class="hover:underline">Address</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
