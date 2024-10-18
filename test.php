83db71e0964012d12ab40e04c962520fd3d16b26
Kevin Elvio <62099198+KevinElvio@users.noreply.github.com>
2024-10-18 12:50:11 +0700

    Fix

---
+++ test.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include './ui-components/head.php' ?>
+
+<body class="bg-black font-poppins">
+<div class="bg-yellow-500">
+ <h1 class="ml-20">HALOI</h1>
+</div>
+</body>
+
+</html>
