da01918b781f45cc380cd6e56b49c35ea443702d
Rasthian <rasthiandwiki@gmail.com>
2024-10-18 13:49:27 +0700

    fix profilepage

--- pageProfile.php
+++ pageProfile.php
@@ -7,26 +7,29 @@
 <body>
     <div class="fixed px-10 py-5 w-full flex flex-row justify-between sm:ml-[20%]">
         <span class="text-xl cursor-pointer bg-blue-400 text-white px-4 py-1 rounded-md sm:hidden" id="menu-icon">☰</span>
-        <div class="text-3xl font-bold">Setting</div>
+        
     </div>
 
-    <div class="menu flex flex-col sm:flex-row">
-        <div id="dropdown" class="absolute h-auto w-full mt-16 mb-60 hidden sm:flex sm:flex-col sm:justify-start sm:w-[20%] sm:h-full sm:my-7 bg-cyan-100 font-bold text-white shadow-lg p-4 rounded-lg">
-            <div class="text-xl font-bold text-blue-400 mb-4">SiPaling Sehat</div>
-            <button class="mb-2 p-2 bg-blue-300 rounded-md text-left" id="profile-button">Profile</button>
-            <button class="mb-2 p-2 bg-blue-300 rounded-md text-left" id="account-button">Account</button>
-            <button class="mb-2 p-2 bg-red-500 rounded-md text-left sm:mt-[180%]">Logout</button>
+    <div class="menu flex flex-col sm:flex-row ml-6">
+        <div id="dropdown" class="absolute h-auto w-full mt-16 mb-60 hidden sm:flex sm:flex-col sm:justify-start sm:w-[20%] sm:h-full sm:my-7 bg-blue-100 font-bold text-white shadow-lg p-4 rounded-lg">
+            <div class="text-xl font-bold text-blue-600 mb-4">SiPaling Sehat</div>
+            <button class="mb-2 p-2 bg-gray-400 rounded-md text-left hover:bg-blue-300" id="profile-button">Profile</button>
+            <button class="mb-2 p-2 bg-gray-400 rounded-md text-left hover:bg-blue-300" id="account-button">Account</button>
+            <button class="mb-2 p-2 bg-red-500 rounded-md text-left hover:bg-red-600 sm:mt-[180%]">Logout</button>
         </div>
 
         <div class="content1 mt-20 pl-7 sm:ml-[20%] sm:w-full ">
             <div class="sub-content1 w-full flex flex-col justify-center sm:flex-row sm:justify-evenly sm:items-center items-center">
                 <div class="picture flex flex-col items-center">
+                    <div class="title-pos w-full flex justify-center mb-9 sm:justify-start">
+                    <div class="text-3xl font-bold">Setting</div>
+                    </div>
                     <div class="title-content text-xl my-5">Profile Picture</div>
                     <img src="assets/images/profile.jpeg" class="rounded-full w-64" alt="">
                 </div>
-                <div class="button-sub-content1 w-full flex flex-col items-center sm:flex-row sm:ml-28 ">
-                    <button class="change-pict text-white font-medium w-[80%] sm:w-64 h-8 bg-blue-400 flex justify-center items-center my-4 rounded-lg">Change Picture</button>
-                    <button class="change-pict font-medium w-[80%] h-8 sm:w-64 text-red-500 flex justify-center items-center my-4">Delete Picture</button>
+                <div class="button-sub-content1 w-full flex flex-col items-center sm:flex-row sm:ml-28 gap-4">
+                    <button class="change-pict text-white font-medium w-[80%] sm:w-64 h-8 bg-blue-400 hover:bg-white hover:text-blue-400 flex justify-center items-center my-4 rounded-lg">Change Picture</button>
+                    <button class="change-pict font-medium w-[80%] h-8 sm:w-64 text-red-500 hover:text-white hover:bg-red-500 flex justify-center items-center my-4 rounded-lg">Delete Picture</button>
                 </div>
 
             </div>
@@ -44,6 +47,9 @@
                 <label class="text-gray-400">About Me:</label>
                 <textarea id="about" name="about" placeholder="Enter about you" rows="4"
                     class="w-full my-2 px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"></textarea>
+                <div class="button-position w-full flex justify-end">
+                <button class="change-pict font-medium w-[80%] h-8 sm:w-64 text-white bg-gray-700 hover:text-white hover:bg-gray-400 flex justify-center items-center my-4 rounded-lg">Save Change</button>
+                </div>
             </div>
         </div>
 
@@ -55,6 +61,9 @@
             <label class="text-gray-400">Password New:</label>
             <input type="password" id="new-password" name="new-password" placeholder="Enter your new password"
                 class="w-full my-2 px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent">
+                <div class="button-position w-full flex justify-end">
+                <button class="change-pict font-medium w-[80%] h-8 sm:w-64 text-white bg-gray-700 hover:text-white hover:bg-gray-400 flex justify-center items-center my-4 rounded-lg">Save Change</button>
+                </div>
         </div>
     </div>
 
