84077471bd9f90284ed2fb37d19eeeb41297758e
Kevin Elvio <62099198+KevinElvio@users.noreply.github.com>
2024-10-25 11:52:30 +0700

    Fix Title

--- ui-components/head.php
+++ ui-components/head.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Pribadi Anak</title>
+    <title>SiPalingSehat - Ayo Cegah Stunting</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <script src="https://cdn.tailwindcss.com"></script>
